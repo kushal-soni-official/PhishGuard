fe08755f1bf9e3d2a7d5a6d4813e05ff56ba39f1
Kushal Soni <kushalsoniofficial@gmail.com>
2026-04-05 11:52:40 -0700

    Delete backend directory

--- backend/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'dev_super_secret_phishguard_key')
-    DEBUG = True
-    MAX_CONTENT_LENGTH = 16 * 1024 * 1024 # 16MB max upload
--- backend/routes/web.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-web_bp = Blueprint('web_bp', __name__)
-
-@web_bp.route('/')
-def dashboard():
-    return render_template('index.html')
